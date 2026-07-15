0393ebc1734399d82cc2dd84366307ec769c6e91
thanhtran0312 <151376396+thanhtran0312@users.noreply.github.com>
2026-07-15 13:03:21 +0200

    Update loss.py

--- losses/loss.py
+++ losses/loss.py
@@ -1,3 +1,5 @@
+# this script's not used. i decided to use torch.nn.NLLLoss instantiation instead.
+
 import torch.nn as nn
 import torch.nn.functional as F 
 
@@ -7,3 +9,12 @@ class NLLLoss(nn.Module):
 
     def forward(self, pred, target):
         loss = F.nll_loss(pred, target.long())
+        return loss
+        
+def compute_loss(cfg, logits, target, loss_dict=None):
+    tot_loss = 0.
+    pred = F.log_softmax(logits, dim=-1).view(-1, int(cfg['n_classes']))
+    criterion = NLLLoss()          # instantiate the actual class
+    loss = criterion(pred, target.long())   # calls NLLLoss.forward
+    tot_loss += loss
+    return tot_loss
